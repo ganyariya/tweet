0f9f88c0cec003c64e1ccc55985194b7246b7e84
ganariya <ganariya2525@gmail.com>
2021-06-09 09:56:51 +0900

    :sparkles: endless type

--- tweet/main.py
+++ tweet/main.py
@@ -1,4 +1,6 @@
+from os import stat
 from typing import Final
+from rich.console import Console
 
 import typer
 import twitter
@@ -7,16 +9,30 @@ from tweet import settings
 APP_NAME: Final[str] = 'tweet'
 app: Final[typer.Typer] = typer.Typer()
 api: Final[twitter.Api] = twitter.Api(consumer_key=settings.CONSUMER_TOKEN, consumer_secret=settings.CONSUMER_SECRET, access_token_key=settings.ACCESS_TOKEN, access_token_secret=settings.ACCESS_SECRET)
+console: Final[Console] = Console()
 
 
 @app.command()
-def tweet(status: str):
-    """ツイートする.
+def tweet(status: str) -> None:
+    """Tweet
 
     :param status: str
     :return: None
     """
+    print(status)
     api.PostUpdate(status=status)
+
+@app.command()
+def endless() -> None:
+    """
+    :return: None
+    """
+    while True:
+        status = console.input(f':bird: < What\'s happening?  ')
+        if len(status) == 0:
+            continue
+        api.PostUpdate(status=status)
+        console.print(f':bird: < Tweeted! [bold]“{status}”[/bold]')
 
 
 if __name__ == '__main__':
--- tweet/settings.py
+++ tweet/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 HOME_DIR: Final[str] = os.environ.get("HOME")
 ENV_PATH: Final[str] = os.path.join(HOME_DIR, '.twitter-env')
 load_dotenv(ENV_PATH)
+
 CONSUMER_TOKEN: Final[str] = os.environ.get("CONSUMER_TOKEN")
 CONSUMER_SECRET: Final[str] = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN: Final[str] = os.environ.get("ACCESS_TOKEN")
